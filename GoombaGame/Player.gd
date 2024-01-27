dcf6831a8fa51d79cdf9daf843c91362df33b403
SenaRomanova <gxkseniaromanova@gmail.com>
2024-01-27 18:43:10 -0500

    -

--- GoombaGame/Player.gd
+++ GoombaGame/Player.gd
@@ -4,12 +4,17 @@ const speed = 400.0
 const friction = 8000.0
 const accel = 4000.0
 
+var col_amount = 0
+
 func _physics_process(delta):
 	
 	var input_direction = Input.get_vector("left_1", "right_1", "up_1", "down_1") 
 	
 	player_movement(input_direction, delta)
 	move_and_slide()
+	
+	if col_amount == 3:
+		get_tree().reload_current_scene()
 	
 
 func player_movement(input, delta):
@@ -26,3 +31,5 @@ func player_movement(input, delta):
 		var collision = get_slide_collision(i)
 		if collision.get_collider() is Player2:
 			collision.get_collider().queue_free()
+			col_amount += 1
+	
