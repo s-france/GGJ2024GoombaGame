67aa4df5963d5dca7cbd4587ec1ed400ee118cd7
s-france <sfrance1218@gmail.com>
2024-01-27 14:13:09 -0500

    Update P2Controller.gd

--- GoombaGame/P2Controller.gd
+++ GoombaGame/P2Controller.gd
@@ -5,6 +5,8 @@ extends Node2D
 var input_direction : float = 0
 var jump : bool = false
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
